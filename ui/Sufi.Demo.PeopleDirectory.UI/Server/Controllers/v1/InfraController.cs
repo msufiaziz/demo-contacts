6619ea127622a6aa69048235df3ee33a1e0ebf00
Sufi Aziz <msufiaziz@gmail.com>
2024-03-30 20:52:41 +0800

    Added serilog for application logging to file.

--- ui/Sufi.Demo.PeopleDirectory.UI/Server/Controllers/v1/InfraController.cs
+++ ui/Sufi.Demo.PeopleDirectory.UI/Server/Controllers/v1/InfraController.cs
@@ -6,8 +6,20 @@ namespace Sufi.Demo.PeopleDirectory.UI.Server.Controllers.v1
 	[ApiVersion(1.0)]
 	public class InfraController : BaseApiController<InfraController>
 	{
+		private readonly ILogger<InfraController> _logger;
+
+		public InfraController(ILogger<InfraController> logger)
+		{
+			_logger = logger;
+		}
+
 		[Route("ping")]
         [HttpGet]
-        public IActionResult Ping() => Ok();
+        public IActionResult Ping()
+		{
+			_logger.LogInformation("InfraController.Ping method called.");
+
+			return Ok();
+		}
     }
 }
