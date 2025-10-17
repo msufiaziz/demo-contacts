6c48cc41395399d5241588c50c0188df2307d27c
Sufi Aziz <msufiaziz@gmail.com>
2025-10-18 06:50:52 +0800

    Updates.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Welcome to My Demo Application!
-This application is to demonstrate my full stack skills in building a working sample of web application, with CRUD operations that persist data in database (SQL Server Express in this case).
+This application is to demonstrate my full stack skills in building a working sample of web application, with CRUD operations that persist data in a database.
 
 # Technology used
 - Blazor
-- SQL Server Express
+- Postgres SQL
--- ui/Sufi.Demo.PeopleDirectory.UI/Server/Controllers/v2/InfraController.cs
+++ ui/Sufi.Demo.PeopleDirectory.UI/Server/Controllers/v2/InfraController.cs
@@ -3,9 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Sufi.Demo.PeopleDirectory.UI.Server.Controllers.v2
 {
+	/// <summary>
+	/// Provides API endpoints for infrastructure-related operations.
+	/// </summary>
+	/// <remarks>This controller is part of the infrastructure layer and includes endpoints for monitoring and diagnostics.</remarks>
 	[ApiVersion(2.0)]
 	public class InfraController : BaseApiController<InfraController>
 	{
+		/// <summary>
+		/// Simulates a ping to check if the server is responsive.
+		/// </summary>
+		/// <returns></returns>
 		[Route("ping")]
 		[HttpGet]
 		public IActionResult Ping() => Ok("Server is OK");
